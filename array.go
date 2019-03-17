570d602f24382e2de42ede805a0fc97f011ca249
Huynh Minh Tuan <huynhminhtuan@Huynh-Minh-Tuans-Macbook.local>
2019-03-17 20:02:21 +0700

    more

--- array.go
+++ array.go
@@ -10,9 +10,19 @@ type Array struct {
 
 // MaxInt ..
 func (Array) MaxInt(arr []int) (max int) {
-	for i := range arr {
-		if i > max {
-			max = i
+	for _, v := range arr {
+		if v > max {
+			max = v
+		}
+	}
+	return
+}
+
+// MaxFloat ..
+func (Array) MaxFloat(arr []float64) (max float64) {
+	for _, v := range arr {
+		if v > max {
+			max = v
 		}
 	}
 	return
@@ -20,21 +30,35 @@ func (Array) MaxInt(arr []int) (max int) {
 
 // MinInt ..
 func (Array) MinInt(arr []int) (min int) {
-	for i := range arr {
-		if i < min {
-			min = i
+	for _, v := range arr {
+		if v < min {
+			min = v
+		}
+	}
+	return
+}
+
+// MinFloat ..
+func (Array) MinFloat(arr []float64) (min float64) {
+	for _, v := range arr {
+		if v < min {
+			min = v
 		}
 	}
 	return
 }
 
 // Index ..
-func (Array) Index(arr []interface{}, item interface{}) (index int) {
-	s := reflect.ValueOf(arr)
-	for i := 0; i < s.Len(); i++ {
-		if reflect.DeepEqual(item, s.Index(i).Interface()) == true {
-			index = i
-			break
+func (Array) Index(arr interface{}, item interface{}) (index int) {
+	index = -1
+	switch reflect.TypeOf(arr).Kind() {
+	case reflect.Slice:
+		s := reflect.ValueOf(arr)
+		for i := 0; i < s.Len(); i++ {
+			if reflect.DeepEqual(item, s.Index(i).Interface()) == true {
+				index = i
+				break
+			}
 		}
 	}
 	return
